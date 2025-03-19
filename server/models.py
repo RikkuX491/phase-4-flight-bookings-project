1c8017fec04cb4c7ce00071175e37ef7f67949ef
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-19 14:56:52 -0400

    Added Flight model with columns, constraints, validation, __repr__() method in models.py, added seed data in seed.py, initialized migrations data, changed db file name in config.py, and imported Flight model into app.py and seed.py

--- server/models.py
+++ server/models.py
@@ -7,3 +7,21 @@ from sqlalchemy.orm import validates
 from config import db
 
 # Models go here!
+class Flight(db.Model, SerializerMixin):
+    __tablename__ = 'flights'
+
+    id = db.Column(db.Integer, primary_key=True)
+    airline = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    @validates('airline', 'image')
+    def validate_airline(self, column_name, value):
+        if type(value) != str:
+            raise TypeError(f"{column_name} must be a string!")
+        elif len(value) < 5:
+            raise ValueError(f"{column_name} must be at least 5 characters long!")
+        else:
+            return value
+        
+    def __repr__(self):
+        return f"<Flight {self.id} - Airline: {self.airline}, Image: {self.image}>"
