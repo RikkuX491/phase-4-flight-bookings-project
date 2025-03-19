a42fddd6bb53b46a9c8ab4a8405fd0daf7d13d9b
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-19 14:34:30 -0400

    Updated starter code for both front-end and back-end

--- server/models.py
+++ server/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
+# Adding the validates import to use validations
+from sqlalchemy.orm import validates
+
 from config import db
 
 # Models go here!
