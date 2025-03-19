c2858eaeee21f5e19beae9526cb9940bcbd330ae
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-19 15:10:13 -0400

    Added functionality to retrieve flights data for all flights from the Flask server and update the flights state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+// import { Switch, Route } from "react-router-dom";
 
 function App() {
+
+  const [flights, setFlights] = useState([])
+  console.log(flights)
+
+  useEffect(getFlights, [])
+
+  function getFlights(){
+    fetch("/flights")
+    .then(response => response.json())
+    .then(flightsData => setFlights(flightsData))
+  }
+
   return <h1>Project Client</h1>;
 }
 
